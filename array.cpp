678f89801a4e33fff7bcacea6e561d9e2fa9fb47
bhavna yadav <yadavbhavna869@gmail.com>
2026-05-06 23:22:09 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[5],i;
+	cout<<"\n enter a number:";
+	for(i=0;i<5;i++)
+	cin>>a[i];
+	cout<<"enter elements";
+	for(i=0;i<5;i++)
+	cout<<"\t"<<a[i];
+	return 0;
+}
